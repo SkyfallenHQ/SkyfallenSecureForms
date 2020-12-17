c6710583629ee013649af3628e84a7c7c1792be0
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-17 12:35:27 +0300

    Create UpdaterConfiguration.php

---
+++ Configuration/UpdaterConfiguration.php
@@ -0,0 +1,23 @@
+<?php
+/***********************************************************************************/
+/*                          (C) 2020 - Skyfallen                                   */
+/*                    Skyfallen Secure Forms Developed by                          */
+/*                          The Skyfallen Company                                  */
+/*                                                                                 */
+/*                         File Since: SFR-301004                                  */
+/*      This file stores the updater configuration for the application.            */
+/*               This file does not need to be modified by the user.               */
+/*  The data in this file contains data regarding how this app should connect to   */
+/*                     Skyfallen's Software Update Servers                         */
+/***********************************************************************************/
+
+// Set the API Endpoint:
+define("UC_API_ENDPOINT","https://swupdate.theskyfallen.com");
+
+// Set the VERIFIED App Tokens(This ID won't be verified if changed.)
+define("UC_API_APPID","");
+
+// Set the APP Update Seed
+define("UC_API_APPSEED","");
+
+// EOF
