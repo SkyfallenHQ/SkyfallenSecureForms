f4cc4ef27fbad14694c7e124f98bbb1fa15afb59
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-17 12:35:57 +0300

    Create SoftwareUpdater.php

---
+++ SoftwareUpdater.php
@@ -0,0 +1,12 @@
+<?php
+/***********************************************************************************/
+/*                          (C) 2020 - Skyfallen                                   */
+/*                    Skyfallen Secure Forms Developed by                          */
+/*                          The Skyfallen Company                                  */
+/*                                                                                 */
+/*                         File Since: SFR-301006                                  */
+/*      This file stores the updater configuration for the application.            */
+/*               This file does not need to be modified by the user.               */
+/*  The data in this file contains data regarding how this app should connect to   */
+/*                     Skyfallen's Software Update Servers                         */
+/***********************************************************************************/
