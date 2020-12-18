4d57721c13908e273cf7dbd1a1b3f6e28b9fb6d3
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-18 18:27:53 +0300

    Remove error reporting from index file and include the install page if not yet installed

--- index.php
+++ index.php
@@ -8,17 +8,21 @@
 /* This file is where all requests are redirected to. All file inclusions are here.*/
 /***********************************************************************************/
 
-error_reporting(E_ALL);
-ini_set("display_errors",1);
-ini_set("display_startup_errors",1);
-
-
 // Define the application's ABSOLUTE FS Path
 define("SSF_ABSPATH", dirname(__FILE__));
 
 // Include All Configuration Files
-include_once SSF_ABSPATH."/Configuration/SecureFormDatabaseConfiguration.php";
 include_once SSF_ABSPATH."/Configuration/UpdaterConfiguration.php";
+include_once SSF_ABSPATH."/thisversion.php";
+
+
+// Unless the installation was complete, the Database Config should exist, if not, run the install.
+if((@include_once SSF_ABSPATH . "/Configuration/SecureFormDatabaseConfiguration.php") === false){
+    // Include the install file
+    include_once SSF_ABSPATH."";
+    die();
+}
+
 
 // Include all the classes
 include_once SSF_ABSPATH."/FunctionSets/DBSettings.php";
