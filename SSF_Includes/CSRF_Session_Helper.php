886cb4e909e1b3d7406fac4ec02664457d4104ad
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-20 23:00:12 +0300

    Helper for CSRF

---
+++ SSF_Includes/CSRF_Session_Helper.php
@@ -0,0 +1,20 @@
+<?php
+/***********************************************************************************/
+/*                          (C) 2020 - Skyfallen                                   */
+/*                    Skyfallen Secure Forms Developed by                          */
+/*                          The Skyfallen Company                                  */
+/*                                                                                 */
+/*                         File Since: SFR-301070                                  */
+/*           This file prepares the session for use of CSRF Tokens                 */
+/***********************************************************************************/
+
+// Check if SESSION has CSRF started
+
+if(!isset($_SESSION["csrf_codes"])){
+
+    // We are not ready to handle CSRF Functions
+    // Initialize an empty array
+
+    $_SESSION["csrf_codes"] = array();
+
+}
