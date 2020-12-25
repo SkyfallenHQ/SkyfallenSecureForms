f3271f1cb275582b0518263aee83d2f26d616b71
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-25 23:27:17 +0300

    Save form responses

--- static/js/form.js
+++ static/js/form.js
@@ -21,16 +21,36 @@ function submitForm() {
 
     if(validateFields()){
 
+        var encryptor = new JSEncrypt();
+
+        encryptor.setPublicKey(publicEncryptionKey);
+
+        console.log(publicEncryptionKey);
+
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.open("POST", web_url+"respond", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        var post_params = "csrf_id="+document.getElementsByName("csrf_id")[0].value+"&csrf_token="+document.getElementsByName("csrf_token")[0].value+"&form_id="+current_form_id+"&respondent_id="+respondentID;
+
         $(".formfield").each(function (index,obj) {
 
-            if(!obj.value){
+            console.log(encryptor.encrypt(obj.value));
 
-                status = false;
-
-                return;
-            }
+            post_params = post_params + "&field_"+index.toString()+"="+encryptor.encrypt(obj.value);
 
         })
+
+        xhttp.send(post_params);
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                $(document).ready(function () {
+                    document.getElementById("body-wrap").style.display = "none";
+                    document.getElementById("responded").style.display = "initial";
+                });
+            }
+        };
 
     } else {
 
