7bfacfeab5db1f68e487aa907292b5f74e9d6a90
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-26 14:57:20 +0300

    Add software update to the menu

--- views/userpage.php
+++ views/userpage.php
@@ -40,6 +40,9 @@ function render_dashboard(){
                 <div class="user-col">
                     <a href="<?php the_pageurl("accounts/logout"); ?>"><h3>Logged in as <?php echo USERNAME; ?>. Log out?</h3></a>
                 </div>
+                <div class="user-col">
+                    <a href="<?php the_pageurl("updates"); ?>"><h3>Software Update</h3></a>
+                </div>
             </div>
         </div>
         <div class="forms-overview">
