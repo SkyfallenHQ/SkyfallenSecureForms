77ad91adc3f573bbd36b55f6b1151dde81b09099
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-26 15:12:53 +0300

    Version Sign off --

--- thisversion.php
+++ thisversion.php
@@ -1,5 +1,5 @@
 <?php
-define("THIS_VERSION","SFR-301001");
-define("THIS_VERSION_NICKNAME", "Galactus");
+define("THIS_VERSION","SFR-301170");
+define("THIS_VERSION_NICKNAME", "Adriatic");
 define("THIS_VERSION_BRANCH","Release Candidate");
 define("VERSION_PROVIDER","Skyfallen Software Distribution");
