e3fa7c6b2da1d3195dae63895d2361999de03fc8
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-26 13:07:10 +0300

    Change Key Generation

--- SSF_Includes/RSA_Helper.php
+++ SSF_Includes/RSA_Helper.php
@@ -14,24 +14,11 @@ defined("SSF_ABSPATH") or die("Hmmmm...");
 // Check if key file exists for the current user
 if(ISLOGGEDIN && !file_exists(SSF_ABSPATH."/DataSecurity/RSA_KEYS/".USERNAME."-key.php")){
 
-    // Files doesn't exist, so create a new keypair.
-    $config = array(
-        "digest_alg" => "sha512",
-        "private_key_bits" => 4096,
-        "private_key_type" => OPENSSL_KEYTYPE_RSA,
-    );
+    // Create a new keypair
+    $keyPair = \ParagonIE\EasyRSA\KeyPair::generateKeyPair(4096);
 
-    // Create the private and public key
-    $res = openssl_pkey_new($config);
-
-    // Extract the private key from $res to $privKey
-    openssl_pkey_export($res, $privKey);
-
-    // Extract the public key from $res to $pubKey
-    $pubKey = openssl_pkey_get_details($res);
-    $pubKey = $pubKey["key"];
-
-
+    $privKey = $keyPair->getPrivateKey()->getKey();
+    $pubKey = $keyPair->getPublicKey()->getKey();
 
     // Open the key file
     $keyFile = fopen(SSF_ABSPATH."/DataSecurity/RSA_KEYS/".USERNAME."-key.php","w+");
