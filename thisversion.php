2a5060a6140f12ec95b081221de680ef86ad852a
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-26 15:00:36 +0300

    New version fields

--- thisversion.php
+++ thisversion.php
@@ -1,3 +1,5 @@
 <?php
 define("THIS_VERSION","SFR-301001");
+define("THIS_VERSION_NICKNAME", "Galactus");
+define("THIS_VERSION_BRANCH","Release Candidate");
 define("VERSION_PROVIDER","Skyfallen Software Distribution");
