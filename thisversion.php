574ccf3d90d02c4c1ba48a377f8c0c83b9b35a5d
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-12-27 19:45:14 +0300

    Push version string

--- thisversion.php
+++ thisversion.php
@@ -1,5 +1,5 @@
 <?php
-define("THIS_VERSION","SFR-301184");
+define("THIS_VERSION","SFR-301198");
 define("THIS_VERSION_NICKNAME", "Adriatic");
-define("THIS_VERSION_BRANCH","Release Candidate");
+define("THIS_VERSION_BRANCH","Stable Release");
 define("VERSION_PROVIDER","Skyfallen Software Distribution");
