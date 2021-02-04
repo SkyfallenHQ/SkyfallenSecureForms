f380cd67566d22eeadec2b15521d5552cd53409c
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-02-04 21:55:56 +0300

    Push version

--- thisversion.php
+++ thisversion.php
@@ -1,5 +1,5 @@
 <?php
-define("THIS_VERSION","SFR-301270");
-define("THIS_VERSION_NICKNAME", "Infinitum");
-define("THIS_VERSION_BRANCH","Stable Release");
-define("VERSION_PROVIDER","Skyfallen Software Distribution");
+define("THIS_VERSION","SFR-301286");
+define("THIS_VERSION_NICKNAME", "Fortuitous");
+define("THIS_VERSION_BRANCH","SDC Beta");
+define("VERSION_PROVIDER","Skyfallen Worldwide Developer Relations");
