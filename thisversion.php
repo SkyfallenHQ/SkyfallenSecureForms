797e1406597e17b9527c26a9739b68d9495fdf40
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-09-02 18:02:57 +0300

    Update thisversion.php

--- thisversion.php
+++ thisversion.php
@@ -1,5 +1,5 @@
 <?php
-define("THIS_VERSION","SFR-301286");
-define("THIS_VERSION_NICKNAME", "Fortuitous");
-define("THIS_VERSION_BRANCH","SDC Beta");
+define("THIS_VERSION","SFR-301287");
+define("THIS_VERSION_NICKNAME", "Fortuitous Service Pack 2021Q4 1");
+define("THIS_VERSION_BRANCH","SDC_Beta");
 define("VERSION_PROVIDER","Skyfallen Worldwide Developer Relations");
